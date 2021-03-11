aaa8846ed32864e83a436fba535faa25b0a45268
AnkurTomarTomar <ankurtomar79@gmail.com>
2021-03-12 02:28:03 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        This is run time change
+        React App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>
+        I was changed by feature branch!
+      </p>
     </div>
   );
 }
